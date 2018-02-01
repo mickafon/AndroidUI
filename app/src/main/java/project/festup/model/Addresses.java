592c2feb02ba8c19ee45e483e47a080b5f236727
aimee-elks <aimee.elmkies@outlook.com>
2018-02-01 18:39:08 +0100

    Commit 01-02
    Modif Widget

---
+++ app/src/main/java/project/festup/model/Addresses.java
@@ -0,0 +1,73 @@
+package project.festup.model;
+
+/**
+ * Created by aimee on 01/02/2018.
+ */
+
+public class Addresses {
+
+    int id;
+    String name;
+    String additional;
+    String street;
+    String postal;
+    String city;
+
+    public Addresses(int id, String name, String additional, String street, String postal, String city) {
+        this.id = id;
+        this.name = name;
+        this.additional = additional;
+        this.street = street;
+        this.postal = postal;
+        this.city = city;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAdditional() {
+        return additional;
+    }
+
+    public void setAdditional(String additional) {
+        this.additional = additional;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getPostal() {
+        return postal;
+    }
+
+    public void setPostal(String postal) {
+        this.postal = postal;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+}
